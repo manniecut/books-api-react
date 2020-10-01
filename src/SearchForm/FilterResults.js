8e2ccc7cae8f426336bf0c2c987a62168dc104b8
Mannie Cutean <68138888+manniecut@users.noreply.github.com>
2020-10-01 14:55:35 -0400

    selected filters passing through

--- src/SearchForm/FilterResults.js
+++ src/SearchForm/FilterResults.js
@@ -2,27 +2,29 @@ import React, { Component } from 'react';
 import './FilterResults.css';
 
 class FilterResults extends Component {
-    handlePType() {
-        this.props.handleUpdate(target.print_type.value)
+    handlePType(value) {
+        console.log('handled P ' + value)
     }
-    handleBType() {
-        this.props.handleUpdate(target.book_lang.option)
+    handleBType(value) {
+        console.log('handled B ' + value)
     }
+
+
     render() {
         return (
-            <form id="filters" className="filters">
+            <div id="filters" className="filters">
                 Print Type:
-                <select name="print_type" id="print_type" onChange={this.handlePType}>
+                <select name="print_type" id="print_type" onChange={e => this.handlePType(e.target.value)}>
                     <option value="all">All</option>
                     <option value="books">Books</option>
                     <option value="magazines">Magazines</option>
                 </select>
                Book Language:
-                <select name="book_lang" id="book_lang" onChange={this.handleBType}>
+                <select name="book_lang" id="book_lang" onChange={e => this.handleBType(e.target.value)}>
                     <option value="en">English</option>
                     <option value="fr">French</option>
                 </select>
-            </form>
+            </div>
         )
     }
 }
